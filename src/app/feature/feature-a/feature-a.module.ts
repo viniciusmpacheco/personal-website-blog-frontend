a6891759fa1f77833c897e0ebf1112654c883230
Vinicius de Moraes Pacheco <viniciusdemoraespacheco@gmail.com>
2024-02-14 21:20:10 -0300

    Initial project structure creation

---
+++ src/app/feature/feature-a/feature-a.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FeatureARoutingModule } from './feature-a-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FeatureARoutingModule
+  ]
+})
+export class FeatureAModule { }
