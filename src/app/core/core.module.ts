871758a55704214dad99b968afd8f8159a0531ee
Vinicius de Moraes Pacheco <viniciusdemoraespacheco@gmail.com>
2024-02-15 22:10:58 -0300

    Main components creation

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { CoreRoutingModule } from './core-routing.module';
+import { MainNavbarComponent } from './components/main-navbar/main-navbar.component';
+import { MainFooterComponent } from './components/main-footer/main-footer.component';
+import { MainBannerComponent } from './components/main-banner/main-banner.component';
+import { MainClockComponent } from './components/main-clock/main-clock.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainNavbarComponent,
+    MainFooterComponent,
+    MainBannerComponent,
+    MainClockComponent
+  ],
   imports: [
-    CommonModule,
-    CoreRoutingModule
+    CommonModule
+  ],
+  exports: [
+    MainNavbarComponent,
+    MainFooterComponent,
+    MainBannerComponent,
+    MainClockComponent
+
   ]
 })
 export class CoreModule { }
