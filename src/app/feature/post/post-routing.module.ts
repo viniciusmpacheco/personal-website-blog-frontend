0e5abe0488c5bf6b59adac48578a0806c72a972d
Vinicius de Moraes Pacheco <viniciusdemoraespacheco@gmail.com>
2024-02-19 13:52:02 -0300

    Basic structure of post information page created

--- src/app/feature/post/post-routing.module.ts
+++ src/app/feature/post/post-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostReadComponent } from './pages/post-read/post-read.component';
+import { PostInfoComponent } from './pages/post-info/post-info.component';
 
 const routes: Routes = [
-  { path: "post", component: PostReadComponent }
+  { path: "post", component: PostReadComponent },
+  { path: "post-info", component: PostInfoComponent }
 ];
 
 @NgModule({
